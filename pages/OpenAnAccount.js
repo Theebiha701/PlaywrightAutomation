a6c55cad0fef0faf2b7ba373b294e0c715219e72
Theebiha <theebiha@gmail.com>
2025-08-19 12:42:51 +0530

    commit

--- pages/OpenAnAccount.js
+++ pages/OpenAnAccount.js
@@ -4,7 +4,7 @@ exports.OpenAnAccount =
 class OpenAnAccount{
     constructor(page){
         this.page = page;
-        this.purpleTransactorTitle = this.page.locator("//h1[contains(text(),'Purple Transactor Accountt.')]");
+        this.purpleTransactorTitle = this.page.locator("//h1[contains(text(),'Purple Transactor Account.')]");
         this.openAnAccountButton = this.page.locator('//a[text()="Open an account"]');
         this.activateYourCardButton = this.page.locator('//a[text()="Activate your card"]');
 
